2bfed9a7707c1bd129f5a0debf03bafe07abe60f
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-07-14 14:31:33 +0900

    revise

--- tfjson.py
+++ tfjson.py
@@ -1,7 +1,9 @@
 import json
 import os, sys
 
-target=['/subgraphs/tensors/quantization/min','/operator_codes','/subgraphs/inputs', '/subgraphs/outputs']
+target=['/buffers/data','/operator_codes','/subgraphs/inputs', '/subgraphs/outputs']
+result=[[] for i in range(len(target))]
+
 def findlist(node_name, path, node, targets):
     ty_node = type(node)
     #print(path,node_name,ty_node)
@@ -10,7 +12,9 @@ def findlist(node_name, path, node, targets):
             findlist(k, path+'/'+k, node[k], targets)
     elif ty_node is list:
         if path in targets:
-            print(path,node)
+#            print(path,node)
+            rindx = targets.index(path)
+            result[rindx].append(node)
         else:
             for n in node:
                 findlist(node_name, path, n, targets)
@@ -18,3 +22,10 @@ def findlist(node_name, path, node, targets):
 with open('detect.json') as r:
     j = json.load(r)
     findlist('','',j,target)
+    for i,r in enumerate(result):
+        if len(r)>100:
+            for j in r:
+                print(target[i],len(j))
+        else:
+            print(target[i],r)
+
