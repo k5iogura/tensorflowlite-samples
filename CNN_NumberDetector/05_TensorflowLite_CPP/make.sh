83be1f4707d27522b5e9bc640252b1c96cb020bb
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-07-20 19:54:45 +0900

    revise

--- CNN_NumberDetector/05_TensorflowLite_CPP/make.sh
+++ CNN_NumberDetector/05_TensorflowLite_CPP/make.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -v
+TENSORFLOW="../../tensorflow"
 if [ ! -d tensorflow ];then
-    echo link tensorflow
-    ln -s ~/tensorflow/tensorflow .
+    if [ ! -d ../../tensorflow/tensorflow ];then
+        echo submodule update
+        pushd ../../tensorflow;git submodule update
+        popd
+    fi
+    if [ -d ../../tensorflow/tensorflow ];then
+        echo link tensorflow to current
+        ln -s ../../tensorflow/tensorflow .
+    fi
 fi
 gcc main.cpp -I. -I./tensorflow -I./tensorflow/lite/tools/make/downloads -I./tensorflow/lite/tools/make/downloads/eigen -I./tensorflow/lite/tools/make/downloads/absl -I./tensorflow/lite/tools/make/downloads/gemmlowp -I./tensorlow/lite/tools/make/downloads/neon_2_sse -I./tensorflow/lite/tools/make/downloads/farmhash/src -I./tensorflow/lite/tools/make/downloads/flatbuffers/include -std=c++11 -lstdc++ -ltensorflow-lite -L./ -lm -pthread `pkg-config --libs opencv`
