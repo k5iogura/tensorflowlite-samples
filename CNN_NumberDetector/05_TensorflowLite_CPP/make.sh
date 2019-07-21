e9367c70813545d767f2b263743455ad3bf1ee0d
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-07-21 21:10:04 +0900

    revise

--- CNN_NumberDetector/05_TensorflowLite_CPP/make.sh
+++ CNN_NumberDetector/05_TensorflowLite_CPP/make.sh
@@ -3,11 +3,9 @@ TENSORFLOW="../../tensorflow"
 if [ ! -d tensorflow ];then
     if [ ! -d ../../tensorflow/tensorflow ];then
         echo submodule update
-        pushd ../../tensorflow
+        pushd ../../
         git submodule init
         git submodule update
-        ./tensorflow/lite/tools/make/download_dependencies.sh
-        ./tensorflow/lite/tools/make/build_lib.sh
         popd
     fi
     if [ -d ../../tensorflow/tensorflow ];then
@@ -15,4 +13,11 @@ if [ ! -d tensorflow ];then
         ln -s ../../tensorflow/tensorflow .
     fi
 fi
+if [ ! -d ./tensorflow/lite/tools/make/downloads/flatbuffers ];then
+    ./tensorflow/lite/tools/make/download_dependencies.sh
+fi
+./tensorflow/lite/tools/make/build_lib.sh
+cp ../../tensorflow/tensorflow/lite/tools/make/gen/linux_x86_64/lib/libtensorflow-lite.a .
+
 gcc main.cpp -I. -I./tensorflow -I./tensorflow/lite/tools/make/downloads -I./tensorflow/lite/tools/make/downloads/eigen -I./tensorflow/lite/tools/make/downloads/absl -I./tensorflow/lite/tools/make/downloads/gemmlowp -I./tensorlow/lite/tools/make/downloads/neon_2_sse -I./tensorflow/lite/tools/make/downloads/farmhash/src -I./tensorflow/lite/tools/make/downloads/flatbuffers/include -std=c++11 -lstdc++ -ltensorflow-lite -L./ -lm -pthread `pkg-config --libs opencv`
+
