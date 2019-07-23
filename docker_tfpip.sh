a131a28ce4bcd2d39096dfe4c9567daad14f29e1
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-07-23 10:41:26 +0900

    revise

---
+++ docker_tfpip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# docker cp docker_tfpip.sh ContainerID:/docker_tfpip.sh
+# 
+apt update && apt install -y openssh-server git curl vim vim-syntax-gtk ctags make cmake swig libjpeg-dev zlib1g-dev python3-dev python3-numpy python3 python python-pip python3-pip libopencv-dev feh python-numpy
+
+pip install numpy opencv-python
+
+git clone https://github.com/tensorflow/tensorflow; cd tensorflow; ./tensorflow/lite/tools/pip_package/build_pip_package.sh
+
+pip install /tmp/tflite_pip/python/dist/tflite_runtime-1.14.0-cp27-cp27mu-linux_x86_64.whl
+
+
+
