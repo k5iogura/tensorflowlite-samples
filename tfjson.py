acdf5e1934125ac4e72e0effc8b160b31f0049e2
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-07-24 17:24:24 +0900

    revise

--- tfjson.py
+++ tfjson.py
@@ -1,67 +1,54 @@
+import os,sys
 import json
-import os, sys
-import numpy as np
 
-def findlist(json_obj,target):
-    result=[[] for i in range(len(target))]
+with open('detect.json') as j: root = json.load(j)
 
-    def json_walk(node_name, path, node, targets):
-        ty_node = type(node)
-        #print(path,node_name,ty_node)
-        if ty_node is dict: # Has child
-            for k in node.keys():
-                json_walk(k, path+'/'+k, node[k], targets)
-        elif ty_node is list:
-            if path in targets:
-                rindx = targets.index(path)
-                result[rindx].append(node)
-            else:
-                for n in node:
-                    json_walk(node_name, path, n, targets)
+# datas_list   = /buffers/data
+cleanup_lambda = lambda i: i.get('data') if i.get('data') is not None else []
+datas_list     = [cleanup_lambda(i) for i in root['buffers']]
 
-    json_walk('','',json_obj,target)
-    return result
+# subgraph     = /subgraphs[0]
+subgraph_dict  = root['subgraphs'][0]
+# inputs       = /subgraphs[0]/inputs
+# outputs      = /subgraphs[0]/outputs
+# operators    = /subgraphs[0]/operators
+# tensors      = /subgraphs[0]/tensors
+inputs_list    = subgraph_dict['inputs']
+outputs_list   = subgraph_dict['outputs']
+operators_list = subgraph_dict['operators']
+tensors_list   = subgraph_dict['tensors']
 
-def walk_tensor(json_obj,start_idx,generators):
-    for j in json_obj:
-        for o_idx in j['outputs']:
-            generators[o_idx] = j['inputs']
+# operator_codes = /operator_codes
+operator_codes_list = root['operator_codes']
 
-if __name__=='__main__':
-    with open('detect.json') as r:
-        target=['/buffers/data','/operator_codes','/subgraphs/inputs', '/subgraphs/outputs']
-        j = json.load(r)
-        inputs    = findlist(j,['/subgraphs/inputs'])
-        outputs   = findlist(j,['/subgraphs/outputs'])
-        tensors   = findlist(j,['/subgraphs/tensors'])
-        operators = findlist(j,['/subgraphs/operators'])
-        buffers   = findlist(j,['/buffers'])
-        print("inputs:",inputs[0][0])
-        print("outputs:",outputs[0][0])
-        print("tensors   N:",len(tensors[0][0]))
-        print("buffers   N:",len(buffers[0][0]))
-        print("operators N:",len(operators[0][0]))
+def generators(operators_list, tensors):
+    # find subgraph input operator index
+    output_operators_idxes = []
+    source_tensor_idxes    = []
+    for ope_idx, ope in enumerate(operators_list):
+        ope_outs = ope['outputs']
+        if len(set(ope_outs+tensors)) != len(ope_outs+tensors):
+            output_operators_idxes.append(ope_idx)
+            source_tensor_idxes.append(ope['inputs'])
+    return output_operators_idxes, source_tensor_idxes
 
-        for idx, start_operator in enumerate(operators[0][0]):
-            start_tensors = start_operator.get('inputs')
-            if inputs[0][0][0] in start_tensors:
-                break
+def consumers(operators_list, tensors):
+    # find subgraph output operator index
+    input_operators_idxes = []
+    distin_tensor_idxes   = []
+    for ope_idx, ope in enumerate(operators_list):
+        ope_ins = ope['inputs']
+        if len(set(ope_ins+tensors)) != len(ope_ins+tensors):
+            input_operators_idxes.append(ope_idx)
+            distin_tensor_idxes.append(ope['outputs'])
+    return input_operators_idxes, distin_tensor_idxes
 
-        for t_idx in start_tensors:
-            print(t_idx)
-            tensor = tensors[0][0][t_idx]
-            bufidx = tensor.get('buffer')
-            bufshp = tensor.get('shape')
-            print('tensor',tensor, bufidx, bufshp, np.prod(bufshp))
-            bufdata= buffers[0][0][bufidx]
-            if bufdata=={}:
-                bN = 0
-            else:
-                bN = len(bufdata.get('data'))
-            print('buffer',bufidx,bufdata,bN)
-
-        #list_generators = [[-1] for i in range(len(tensors[0][0]))]
-        #walk_tensor(operators[0][0], inputs[0][0][0], generators)
-        #for i in range(len(generators)):
-            #print(i,generators[i])
+generators(operators_list, outputs_list)
+ten=[inputs_list]
+while True:
+    ope, ten = consumers(operators_list, ten[0])
+    if len(ope)<=0:break
+    syno = "{} {} {} {}".format(ope,operators_list[ope[0]]['opcode_index'],operators_list[ope[0]]['inputs'],ten)
+    print(syno)
+    #print(ope,operators_list[ope[0]]['builtin_options_type'],operators_list[ope[0]]['inputs'],ten)
 
