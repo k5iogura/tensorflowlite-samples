4bd5ccff40b86188998d529b2545d559d5e90c36
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-07-26 11:57:36 +0900

    revise

--- tfjson.py
+++ tfjson.py
@@ -22,17 +22,32 @@ class graph:
         self.operators_list = subgraph_dict['operators']
         self.tensors_list   = subgraph_dict['tensors']
 
+        self.datas_npy_list = [[]]*len(self.datas_list)
+        self.datas_shp_list = [[]]*len(self.datas_list)
+        self.datas_typ_list = [None]*len(self.datas_list)
+        self.datas_qnt_list = [[]]*len(self.datas_list)
+        for idx in range(len(self.tensors_list)):
+            data_idx = self.tensors_list[idx].get('buffer')
+            data_shp = self.tensors_list[idx].get('shape')
+            data_typ = self.tensors_list[idx].get('type')
+            data_qnt = self.tensors_list[idx].get('quantization')
+            self.datas_shp_list[data_idx] = data_shp
+            self.datas_npy_list[data_idx] = self.get_tensor(idx)
+            self.datas_typ_list[data_idx] = data_typ
+            self.datas_qnt_list[data_idx] = data_qnt
+
         # operator_codes = /operator_codes
         self.operator_codes_list = root['operator_codes']
 
-        self.reset_flag()
+        self.reset_refs()
+        self.order_list     = []
 
-    def reset_flag(self): self.operators_flag = [0]*len(self.operators_list)
+    def reset_refs(self): self.operator_refs = [0]*len(self.operators_list)
 
-    def flag(self, operator_idx):
-        flag = self.operators_flag[operator_idx]
-        self.operators_flag[operator_idx] += 1
-        return flag
+    def refs(self, operator_idx):
+        refs = self.operator_refs[operator_idx]
+        self.operator_refs[operator_idx] += 1
+        return refs
 
     def generators(self, tensors):
         # find subgraph input operator index
@@ -59,7 +74,12 @@ class graph:
     def print_operator(self, operator_idx):
         opcode= self.get_opcode_index(operator_idx)
         o_obj = self.operators_list[operator_idx]
-        print("dist_tensor {} <= operator {}(code {}) = src_tensor {}".format( o_obj['outputs'], operator_idx, opcode, o_obj['inputs']) )
+        print("dist_tensor {} <= operator {}(code {}) = src {} :data_idx {} <= {}".format(
+                o_obj['outputs'], operator_idx, opcode, o_obj['inputs'],
+                [self.tensors_list[i].get('buffer') for i in o_obj['outputs']],
+                [self.tensors_list[i].get('buffer') for i in o_obj['inputs']]
+            )
+        )
 
     def get_opcode_index(self,operator):
         opcode_index = self.operators_list[operator].get('opcode_index')
@@ -79,22 +99,22 @@ class graph:
         typ = self.tensors_list[tensor_idx].get('type')
         bdy = self.datas_list[idx]
         if   typ == 'FLOAT32':
-            if len(bdy)==0: return np.asarray([], np.float32)
+            if len(bdy)==0: return np.zeros(shp, np.float32)
             return np.asarray( [self.list2float(bdy, i, 4) for i in range(0,len(bdy),4)], np.float32 ).reshape(shp)
         elif typ == 'FLOAT16':
-            if len(bdy)==0: return np.asarray([], np.float16)
+            if len(bdy)==0: return np.zeros(shp, np.float16)
             return np.asarray( [self.list2float(bdy, i, 2) for i in range(0,len(bdy),2)], np.float16 ).reshape(shp)
         elif typ == 'INT32':
-            if len(bdy)==0: return np.asarray([], np.int32)
+            if len(bdy)==0: return np.zeros(shp, np.int32)
             return np.asarray( [self.list2int(bdy, i, 4) for i in range(0,len(bdy),4)], np.int32 ).reshape(shp)
         elif typ == 'UINT8':
-            if len(bdy)==0: return np.asarray([], np.uint8)
+            if len(bdy)==0: return np.zeros(shp, np.uint8)
             return np.asarray(bdy, np.uint8).reshape(shp)
         elif typ == 'INT64':
-            if len(bdy)==0: return np.asarray([], np.int64)
+            if len(bdy)==0: return np.zeros(shp, np.int64)
             return np.asarray( [self.list2int(bdy, i, 8) for i in range(0,len(bdy),8)], np.int64 ).reshape(shp)
-        elif typ == None:
-            return np.asarray([], np.uint8)
+        elif typ == None and shp is not None:
+            return np.zeros(shp, np.uint8).reshape(shp)
         assert False, "tensor_idx={} shape:{} type:{} bdy:{}".format(tensor_idx, shp, typ, bdy)
 
     #     Generators      Focus        Consumers
@@ -103,14 +123,15 @@ class graph:
     #                   |        |
     #Tensor ____ ope ___|        |____ ope --- Tensor
     #List                                      List
-    def walk_from(self, tensor_idx, proc=None, verbose=True):
+    def walk_from(self, tensor_idx, func=None, verbose=True):
         operators, tensors = self.generators(tensor_idx)
         for o, t in zip(operators, tensors):
-            if self.flag(o)>0:continue
-            self.walk_from(t, proc, verbose)
+            if self.refs(o)>0:continue
+            self.walk_from(t, func, verbose)
+            self.order_list.append(o)
             if verbose: self.print_operator(o)
 
-            if proc is not None:
+            if func is not None:
                 opcode_index = self.get_opcode_index(o)
                 src_numpy_list = []
                 src_tensors_list = self.operators_list[o].get('inputs')
@@ -119,7 +140,10 @@ class graph:
                 dst_numpy_list = []
                 dst_tensors_list = self.operators_list[o].get('outputs')
                 for t in dst_tensors_list: dst_numpy_list.append(self.get_tensor(t))
-                proc(dst_numpy_list, opcode_index, src_numpy_list)
+                func(dst_numpy_list, opcode_index, src_numpy_list)
+
+    def allocate_graph(self, verbose=True):
+        self.walk_from(self.outputs_list, None, verbose)
 
 def exec_operation(np_dst, opcode_index, np_src):
     print(
@@ -130,5 +154,6 @@ def exec_operation(np_dst, opcode_index, np_src):
     ))
 
 g=graph('detect.json')
-g.walk_from(g.outputs_list, exec_operation, verbose=True)
+#g.walk_from(g.outputs_list, exec_operation, verbose=True)
+g.allocate_graph()
 
