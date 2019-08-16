fbfa175a48cc10cba03175e2d27d8b3dfab0295d
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-08-16 19:28:18 +0900

    revise

--- mnist_train_saving/mnist4ML.py
+++ mnist_train_saving/mnist4ML.py
@@ -2,6 +2,9 @@
 
 # TensowFlowのインポート
 import tensorflow as tf
+#import tensorflow.contrib.eager as tfe
+#tfe.enable_eager_execution()
+
 # MNISTを読み込むためinput_data.pyを同じディレクトリに置きインポートする
 # input_data.pyはチュートリアル内にリンクがあるのでそこから取得する
 # https://tensorflow.googlesource.com/tensorflow/+/master/tensorflow/examples/tutorials/mnist/input_data.py
@@ -24,32 +27,36 @@ print "--- MNISTデータの読み込み開始 ---"
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 print "--- MNISTデータの読み込み完了 ---"
 
+class Net():
+    def __init__(self):
 # 訓練画像を入れる変数
 # 訓練画像は28x28pxであり、これらを1行784列のベクトルに並び替え格納する
 # Noneとなっているのは訓練画像がいくつでも入れられるようにするため
-x = tf.placeholder(tf.float32, [None, 784], name='inputX')
+        self.x = tf.placeholder(tf.float32, [None, 784], name='inputX')
 #tf.identity(x, name='inputY')
 
 # 重み
 # 訓練画像のpx数の行、ラベル（0-9の数字の個数）数の列の行列
 # 初期値として0を入れておく
-W = tf.Variable(tf.zeros([784, 10]))
+        self.W = tf.Variable(tf.zeros([784, 10]))
 
 # バイアス
 # ラベル数の列の行列
 # 初期値として0を入れておく
-b = tf.Variable(tf.zeros([10]))
+        self.b = tf.Variable(tf.zeros([10]))
 
 # ソフトマックス回帰を実行
 # yは入力x（画像）に対しそれがある数字である確率の分布
 # matmul関数で行列xとWの掛け算を行った後、bを加算する。
 # yは[1, 10]の行列
-y = tf.nn.softmax(tf.matmul(x, W) + b, name='outputX')
+        self.y = tf.nn.softmax(tf.matmul(self.x, self.W) + self.b, name='outputX')
+
+net = Net()
 
 # 交差エントロピー
 # y_は正解データのラベル
 y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+cross_entropy = -tf.reduce_sum(y_*tf.log(net.y))
 
 # 勾配硬化法を用い交差エントロピーが最小となるようyを最適化する
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
@@ -68,11 +75,14 @@ sess.run(init)
 # 訓練データは60000点あるので全て使いたいところだが費用つまり時間がかかるのでランダムな100つを使う
 # 100つでも同じような結果を得ることができる
 # feed_dictでplaceholderに値を入力することができる
+saver = tf.train.Saver()
 print "--- 訓練開始 ---"
-for i in range(1000):
+for i in range(100):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_:batch_ys})
+    sess.run(train_step, feed_dict={net.x: batch_xs, y_:batch_ys})
 print "--- 訓練終了 ---"
+saver.save(sess,"./ckpt/mnist.ckpt",global_step=100)
+
 
 # 正しいかの予測
 # 計算された画像がどの数字であるかの予測yと正解ラベルy_を比較する
@@ -80,7 +90,7 @@ print "--- 訓練終了 ---"
 # argmaxは配列の中で一番値の大きい箇所のindexが返される
 # 一番値が大きいindexということは、それがその数字である確率が一番大きいということ
 # Trueが返ってくるということは訓練した結果と回答が同じということ
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(net.y,1), tf.argmax(y_,1))
 
 # 精度の計算
 # correct_predictionはbooleanなのでfloatにキャストし、平均値を計算する
@@ -91,7 +101,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 # テストデータの画像とラベルで精度を確認する
 # ソフトマックス回帰によってWとbの値が計算されているので、xを入力することでyが計算できる
 print "精度"
-print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+print(sess.run(accuracy, feed_dict={net.x: mnist.test.images, y_: mnist.test.labels}))
 
 # 終了時刻
 end_time = time.time()
