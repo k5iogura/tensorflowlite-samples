6d5d1d7ddee14827738ccfc8f8ad3ce460321b02
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-08-20 17:35:05 +0900

    revise

--- mnist_cnn_json/mnist4ML.py
+++ mnist_cnn_json/mnist4ML.py
@@ -2,71 +2,23 @@
 from __future__ import absolute_import, unicode_literals
 import tensorflow.examples.tutorials.mnist.input_data as input_data
 import tensorflow as tf
+from mnist import MNIST
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-
-
-def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
-
-
-def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
-
-
-def conv2d(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
-
-
-def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                          strides=[1, 2, 2, 1], padding='SAME')
-
 
 #SESS
 sess = tf.InteractiveSession()
 
-#PF
-x = tf.placeholder("float", shape=[None, 784], name='inputX')
-y_ = tf.placeholder("float", shape=[None, 10])
-
-#CNN1
-W_conv1 = weight_variable([5, 5, 1, 32])
-b_conv1 = bias_variable([32])
-x_image = tf.reshape(x, [-1, 28, 28, 1])
-h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
-h_pool1 = max_pool_2x2(h_conv1)
-
-#CNN2
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-h_pool2 = max_pool_2x2(h_conv2)
-
-#FC1
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-
-#Dropout
-keep_prob = tf.placeholder("float")
-h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-
-#FC2
-W_fc2 = weight_variable([1024, 10])
-b_fc2 = bias_variable([10])
-y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='outputX')
+net = MNIST()
+y_  = tf.placeholder("float", shape=[None, 10])
 
 #Saver
 saver = tf.train.Saver()
 
 #Train
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y_conv))
+cross_entropy = -tf.reduce_sum(y_ * tf.log(net.y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+correct_prediction = tf.equal(tf.argmax(net.y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
 sess.run(tf.initialize_all_variables())
 
@@ -76,17 +28,18 @@ for i in range(train_count):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
-            x: batch[0], y_: batch[1], keep_prob: 1.0})
+            net.x: batch[0], y_: batch[1], net.keep_prob: 1.0})
         print "step %d, training accuracy %g" % (i, train_accuracy)
-    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+    train_step.run(feed_dict={net.x: batch[0], y_: batch[1], net.keep_prob: 0.5})
 
 print "test accuracy %g" % accuracy.eval(
     feed_dict={
-        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0
+        net.x: mnist.test.images, y_: mnist.test.labels, net.keep_prob: 1.0
     }
 )
 
 # Saving Model
+print("Saving Model Stage")
 saver.save(sess,"./ckpt/mnist.ckpt",global_step=100)
 frzdef = tf.compat.v1.graph_util.convert_variables_to_constants(
     sess,
@@ -105,3 +58,4 @@ tf.train.write_graph(
     as_text=True
 )
 
+print("Fin")
