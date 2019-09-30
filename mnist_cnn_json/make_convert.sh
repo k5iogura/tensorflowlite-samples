91ec802bacd64f23dc2c5dbbc998f60b0eea0b4f
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-09-30 12:58:40 +0900

    revise

--- mnist_cnn_json/make_convert.sh
+++ mnist_cnn_json/make_convert.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -v
-tflite_convert --output_file=mnist_cnn.tflite --graph_def_file=mnist_frozen.pb --inference_type=QUANTIZED_UINT8 --inference_input_type QUANTIZED_UINT8 --input_arrays inputX --output_arrays outputX --std_dev_values 1 --mean_values 127 --default_ranges_min=-50 --default_ranges_max=255
+tflite_convert \
+--output_file=mnist_cnn.tflite \
+--graph_def_file=mnist_frozen.pb \
+--inference_type=QUANTIZED_UINT8 \
+--inference_input_type QUANTIZED_UINT8 \
+--input_arrays inputX \
+--std_dev_values 1 \
+--mean_values 127 \
+--default_ranges_min=-50 \
+--default_ranges_max=255 \
+--output_arrays=\
+outputX,\
+MaxPool,\
+MaxPool_1,\
+Relu,\
+Relu_1,\
+Relu_2,\
+Reshape,\
+add_3
--- mnist_cnn_json/make_convert_std127_mean30.sh
+++ mnist_cnn_json/make_convert_std127_mean30.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -v
-tflite_convert --output_file=mnist_cnn.tflite --graph_def_file=mnist_frozen.pb --inference_type=QUANTIZED_UINT8 --inference_input_type QUANTIZED_UINT8 --input_arrays inputX --output_arrays outputX --std_dev_values 127 --mean_values 30 --default_ranges_min=-50 --default_ranges_max=255
+tflite_convert \
+--output_file=mnist_cnn.tflite \
+--graph_def_file=mnist_frozen.pb \
+--inference_type=QUANTIZED_UINT8 \
+--inference_input_type QUANTIZED_UINT8 \
+--input_arrays inputX \
+--std_dev_values 127 \
+--mean_values 30 \
+--default_ranges_min=-50 \
+--default_ranges_max=255 \
+--output_arrays=\
+outputX,\
+Reshape,\
+Relu,\
+MaxPool,\
+Relu_1,\
+MaxPool_1,\
+Relu_2,\
+add_3,\
