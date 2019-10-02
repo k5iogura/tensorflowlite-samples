ef0c62aa148dfcb934abcb530cda78b05b1753a6
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-10-03 08:49:02 +0900

    revise

--- mnist_cnn_json/flags.py
+++ mnist_cnn_json/flags.py
@@ -1,9 +1,14 @@
 global __floating_infer
+global __relux_info
 class FLAGs:
     def __init__(initval=True):
         global __floating_infer
+        global __relux_info
         __floating_infer = True
+        __relux_info     = False
 
+#
+# Floating inference mode cntorl
     @property
     def floating_infer(self):
         global __floating_infer
@@ -13,4 +18,17 @@ class FLAGs:
     def floating_infer(self,val):
         global __floating_infer
         __floating_infer = val
+
+#
+# Debug info control for RELUx
+    @property
+    def relux_info(self):
+        global __relux_info
+        return __relux_info
+
+    @relux_info.setter
+    def relux_info(self,val):
+        global __relux_info
+        __relux_info = val
+
 flags = FLAGs()
