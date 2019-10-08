39e3787b4bdd71eb5219c647158f852f9b9289ed
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-10-08 09:59:50 +0900

    revise

--- models_zoo/research/opject_detection/node_pb.py
+++ models_zoo/research/opject_detection/node_pb.py
@@ -23,19 +23,19 @@ with detection_graph.as_default():
     tf.import_graph_def(od_graph_def, name='')
 
 
-def search_graph(graph):
-  with graph.as_default():
-    with tf.Session() as sess:
-      # Get handles to input and output tensors
-      ops = tf.get_default_graph().get_operations()
-      all_output_names = {tensor.name for op in ops for tensor in op.outputs}
-      all_input_names  = {tensor.name for op in ops for tensor in op.inputs}
-      all_op_names     = {op.name for op in ops}
-      only_l0_outputs  = { i for i in (all_output_names - all_input_names) if ':0' in i and '/' not in i }
-      only_inputs      = { i for i in (all_input_names - all_output_names) }
-  return only_l0_outputs, only_inputs
+with detection_graph.as_default():
+  with tf.Session() as sess:
+    ops = tf.get_default_graph().get_operations()
+    all_output_tensors = {tensor for op in ops for tensor in op.outputs}
+    all_input_tensors  = {tensor for op in ops for tensor in op.inputs}
+    all_output_names = {tensor.name for op in ops for tensor in op.outputs}
+    all_input_names  = {tensor.name for op in ops for tensor in op.inputs}
+    all_op_names     = {op.name for op in ops}
+    only_l0_outputs  = { i for i in (all_output_names - all_input_names) if ':0' in i and '/' not in i }
+    only_inputs      = { i for i in (all_input_names - all_output_names) }
 
+    feedables        = {i.name for i in all_input_tensors if tf.get_default_graph().is_feedable(i) and '/' not in i.name}
 
-outputs, inputs = search_graph(detection_graph)
-print('only_l0_outputs:{}'.format(outputs))
-print('only_inputs    :{}'.format(inputs))
+print('only_l0_outputs  :{}'.format(only_l0_outputs))
+print('only_inputs      :{}'.format(only_inputs))
+print('feedable tensors :{}'.format(feedables))
