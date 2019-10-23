4bc1d89d4b1c01d920288d6f39591855c46b1aa0
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-10-23 13:09:15 +0900

    revise

--- models_zoo/research/opject_detection/object_detection_pb.py
+++ models_zoo/research/opject_detection/object_detection_pb.py
@@ -14,6 +14,8 @@ from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 import cv2
+from object_detection_utils import *
+from pdb import set_trace
 
 # This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
@@ -22,12 +24,12 @@ from object_detection.utils import ops as utils_ops
 #if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
 #  raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
 
-from object_detection.utils import label_map_util
+#from object_detection.utils import label_map_util
 
 from object_detection.utils import visualization_utils as vis_util
 
 # What model to download.
-MODEL_NAME = 'object_detection/ssd_mobilenet_v1_coco_2018_01_28'
+MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 MODEL_FILE = MODEL_NAME + '.tar.gz'
 DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
@@ -53,7 +55,7 @@ with detection_graph.as_default():
     od_graph_def.ParseFromString(serialized_graph)
     tf.import_graph_def(od_graph_def, name='')
 
-category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+#category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
 
 
 def load_image_into_numpy_array(image):
@@ -65,7 +67,7 @@ def load_image_into_numpy_array(image):
 # image1.jpg
 # image2.jpg
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'object_detection/test_images'
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
 TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
 
 # Size, in inches, of the output images.
@@ -81,6 +83,8 @@ def run_inference_for_single_image(image, graph):
       all_tensor_names = {output.name for op in ops for output in op.outputs}
       tensor_dict = {}
       for key in [
+          'MultipleGridAnchorGenerator/Identity',
+          'concat','concat_1',
           'num_detections', 'detection_boxes', 'detection_scores',
           'detection_classes', 'detection_masks'
       ]:
@@ -115,11 +119,18 @@ def run_inference_for_single_image(image, graph):
           'detection_classes'][0].astype(np.int64)
       output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
       output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      output_dict['pred'] = output_dict['concat']
+      output_dict['conf'] = output_dict['concat_1']
+      output_dict['anch'] = output_dict['MultipleGridAnchorGenerator/Identity']
       if 'detection_masks' in output_dict:
         output_dict['detection_masks'] = output_dict['detection_masks'][0]
   return output_dict
 
 
+def decode(i,pred,priors,var_loc=0.1,var_siz=0.2):
+    gx,gy = var_loc*pred[0,i,:2]*priors[i,2:] + priors[i,:2]
+    gw,gh = np.exp(var_siz*pred[0,i,2:])*priors[i,2:]
+    return gx,gy,gw,gh
 
 for image_path in TEST_IMAGE_PATHS:
   image = Image.open(image_path)
@@ -130,6 +141,39 @@ for image_path in TEST_IMAGE_PATHS:
   image_np_expanded = np.expand_dims(image_np, axis=0)
   # Actual detection.
   output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+  c0=output_dict['pred']
+  c1=output_dict['conf']
+  c2=output_dict['anch']
+  s1=softmax(c1[...,1:])
+  idxs = np.where(s1>0.95)
+  high_conf_regions = c0[0, idxs[1],]
+  high_conf_classes = c1[0, idxs[1],]
+  high_conf_anchors = c2[   idxs[1],]
+  #                defaultBox_wh             * p_xy                      + defaultBox_xy
+  gxy_pred = 0.1 * high_conf_anchors[...,2:] * high_conf_regions[...,:2] + high_conf_anchors[...,:2]
+  #                defaultBox_wh             * np.exp( p_wh )
+  gwh_pred = high_conf_anchors[...,2:] * np.exp(0.2 * high_conf_regions[...,2:])
+  org_h, org_w = image_np.shape[:2]
+  for j in idxs[1]:
+      #xy = c0[0, j, :2]
+      #wh = c0[0, j, 2:]
+      gx,gy,gw,gh = decode(j, c0, c2)
+      classid = np.argmax(s1[0,j])
+      cx,cy,w,h = c2[j,]
+      lx, ly, rx, ry = int(org_w*(cx-w/2.)), int(org_h*(cy-h/2.)), int(org_w*(cx+w/2.)), int(org_h*(cy+h/2.))
+      #lx,ly,rx,ry = int(org_w * (gx-gw/2.)), int(org_h * (gy-gh/2.)), int(org_w * (gx+gw/2.)), int(org_h * (gy+gh/2.))
+      #lx=0 if lx<0 else lx
+      #ly=0 if ly<0 else ly
+      #if lx<0 or ly<0:continue
+      print(lx,ly,rx,ry)
+      if classid == 17: image_np = cv2.rectangle(image_np,(lx,ly),(rx,ry),(0,0,255),1)
+      else: image_np = cv2.rectangle(image_np,(lx,ly),(rx,ry),(255,255,255),1)
+      #set_trace()
+  cv2.imshow('demo',image_np)
+  while True:
+    if cv2.waitKey(10)!=-1:break
+  set_trace()
+  sys.exit(-1)
   # Visualization of the results of a detection.
   vis_util.visualize_boxes_and_labels_on_image_array(
       image_np,
