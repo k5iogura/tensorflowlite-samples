c0e4d8a7c6bfec44445cf1e6afc4043dbc4ed937
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-10-29 17:02:35 +0900

    revise

--- SSD-Tensorflow2/notebooks/object_detection_pb.py
+++ SSD-Tensorflow2/notebooks/object_detection_pb.py
@@ -69,13 +69,26 @@ TEST_IMAGE_PATHS = glob(PATH_TO_TEST_IMAGES_DIR)
 # Size, in inches, of the output images.
 IMAGE_SIZE = (12, 8)
 
-# Generates anchors
+# Generates ssd_anchors as default box
+#  center-X  ,  center-Y  ,  W  ,  H
+#[(38, 38, 1), (38, 38, 1), (4,), (4,)]
+#[(19, 19, 1), (19, 19, 1), (6,), (6,)]
+#[(10, 10, 1), (10, 10, 1), (6,), (6,)]
+#[( 5,  5, 1), ( 5,  5, 1), (6,), (6,)]
+#[( 3,  3, 1), ( 3,  3, 1), (4,), (4,)]
+#[( 1,  1, 1), ( 1,  1, 1), (4,), (4,)]
 from nets import ssd_vgg_300
 ssd_net = ssd_vgg_300.SSDNet()
 net_shape = (300, 300)
 ssd_anchors = ssd_net.anchors(net_shape) # anchors function includes only numpy
+np_anchors = []
+for i in range(len(ssd_anchors)):
+    for cx, cy in   zip(ssd_anchors[i][0].reshape(-1), ssd_anchors[i][1].reshape(-1)):
+        for w, h in zip(ssd_anchors[i][2].reshape(-1), ssd_anchors[i][3].reshape(-1)):
+            np_anchors.append([cx,cy,w,h])
+np_anchors = np.asarray(np_anchors)     # np_anchors.shape (8732,4) correct?
 
-def run_inference_for_single_image(image, graph):
+def run_inference_for_single_image(image, anchors, graph):
   with graph.as_default():
     with tf.Session() as sess:
       # Get handles to input and output tensors
@@ -119,7 +132,7 @@ def run_inference_for_single_image(image, graph):
       flag_background = 1    # which infer image background or not
       conf_threshold  = 0.95
       ij = np.where(pred_conf[:,flag_background:]>conf_threshold)
-      prop_classid = np.argmax(pred_conf[ij[0]],axis=1)
+      prop_classid = np.argmax(pred_conf[ij[0]],axis=1) # prop_classid.shape (8732,21)
       prop_names   = {class_names[i] for i in prop_classid}
 
       # Select hat G proposals via prediction confidence
@@ -131,26 +144,28 @@ def run_inference_for_single_image(image, graph):
           np.reshape(output_dict[ "ssd_300_vgg/block10_box/Reshape"][0],(-1,n_boxdims)),
           np.reshape(output_dict[ "ssd_300_vgg/block11_box/Reshape"][0],(-1,n_boxdims))],
           axis=0)
-      prop_hatgs = pred_hatgs[ij[0]]
-      set_trace()
+      prop_hatgs = pred_hatgs[ij[0]]                     # prop_hatgs.shape (8732,4)
+
+      # Select default box proposals from np_anchors
+      prop_anchors = anchors[ij[0]]
 
       # all outputs are float32 numpy arrays, so convert types as appropriate
-      output_dict['num_detections'] = int(output_dict['num_detections'][0])
-      output_dict['detection_classes'] = output_dict[
-          'detection_classes'][0].astype(np.int64)
-      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-      output_dict['detection_scores'] = output_dict['detection_scores'][0]
-      output_dict['pred'] = output_dict['concat']
-      output_dict['conf'] = output_dict['concat_1']
-      output_dict['anch'] = output_dict['Postprocessor/ExpandDims']
-      if 'detection_masks' in output_dict:
-        output_dict['detection_masks'] = output_dict['detection_masks'][0]
-  return output_dict
+      ret_dict={}
+      ret_dict['num_prop']    = prop_hatgs.shape[0]
+      ret_dict['prop_hatgs']  = prop_hatgs
+      ret_dict['prop_scores'] = pred_conf[ij[0]]
+      ret_dict['prop_anchors']= prop_anchors
+  return ret_dict
 
 
 def decode(i,pred,priors,var_loc=0.1,var_siz=0.2):
     gx,gy = var_loc*pred[0,i,:2]*priors[i,2:] + priors[i,:2]
     gw,gh = np.exp(var_siz*pred[0,i,2:])*priors[i,2:]
+    return gx,gy,gw,gh
+
+def decode2(pred,priors,var_loc=0.1,var_siz=0.2):
+    gx,gy = var_loc*pred[:2]*priors[2:] + priors[:2]
+    gw,gh = np.exp(var_siz*pred[2:])*priors[2:]
     return gx,gy,gw,gh
 
 for image_path in TEST_IMAGE_PATHS:
@@ -159,58 +174,59 @@ for image_path in TEST_IMAGE_PATHS:
   # result image with boxes and labels on it.
   image_np = load_image_into_numpy_array(image)
   # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-  image_np_expanded = np.expand_dims(image_np, axis=0)
+#  image_np_expanded = np.expand_dims(image_np, axis=0)
   image_np_expanded = image_np
   # Actual detection.
-  output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
-  c0=output_dict['pred']
-  c1=output_dict['conf']
-  c2=output_dict['anch'][0]
-  s1=softmax(c1[...,1:])
-  idxs = np.where(s1>0.95)
-  high_conf_regions = c0[0, idxs[1],]
-  high_conf_classes = c1[0, idxs[1],]
-  high_conf_anchors = np.clip(c2[idxs[1],],0., 1.)
-  #                defaultBox_wh             * p_xy                      + defaultBox_xy
-  gxy_pred = 0.1 * high_conf_anchors[...,2:] * high_conf_regions[...,:2] + high_conf_anchors[...,:2]
-  #                defaultBox_wh             * np.exp( p_wh )
-  gwh_pred = high_conf_anchors[...,2:] * np.exp(0.2 * high_conf_regions[...,2:])
+  output_dict = run_inference_for_single_image(image_np_expanded, np_anchors, detection_graph)
+#  c0=output_dict['pred']
+#  c1=output_dict['conf']
+#  c2=output_dict['anch'][0]
+#  s1=softmax(c1[...,1:])
+#  idxs = np.where(s1>0.95)
+#  high_conf_regions = c0[0, idxs[1],]
+#  high_conf_classes = c1[0, idxs[1],]
+#  high_conf_anchors = np.clip(c2[idxs[1],],0., 1.)
+#  #                defaultBox_wh             * p_xy                      + defaultBox_xy
+#  gxy_pred = 0.1 * high_conf_anchors[...,2:] * high_conf_regions[...,:2] + high_conf_anchors[...,:2]
+#  #                defaultBox_wh             * np.exp( p_wh )
+#  gwh_pred = high_conf_anchors[...,2:] * np.exp(0.2 * high_conf_regions[...,2:])
   org_h, org_w = image_np.shape[:2]
-  for j in idxs[1]:
-      #xy = c0[0, j, :2]
-      #wh = c0[0, j, 2:]
-      gx,gy,gw,gh = np.clip(decode(j, c0, c2),0.,1.)
-      classid = np.argmax(s1[0,j])
-      cx,cy,w,h = c2[j,]
-      lx, ly, rx, ry = int(org_w*(cx-w/2.)), int(org_h*(cy-h/2.)), int(org_w*(cx+w/2.)), int(org_h*(cy+h/2.))
-      #lx,ly,rx,ry = int(org_w * (gx-gw/2.)), int(org_h * (gy-gh/2.)), int(org_w * (gx+gw/2.)), int(org_h * (gy+gh/2.))
+#  set_trace()
+  for hatg, prior in zip(output_dict['prop_hatgs'], output_dict['prop_anchors']):
+      #gx,gy,gw,gh = np.clip(decode(j, c0, c2),0.,1.)
+      gx,gy,gw,gh = decode2(hatg, prior)
+#      classid = np.argmax(s1[0,j])
+#      cx,cy,w,h = c2[j,]
+#      lx, ly, rx, ry = int(org_w*(cx-w/2.)), int(org_h*(cy-h/2.)), int(org_w*(cx+w/2.)), int(org_h*(cy+h/2.))
+      lx,ly,rx,ry = int(org_w * (gx-gw/2.)), int(org_h * (gy-gh/2.)), int(org_w * (gx+gw/2.)), int(org_h * (gy+gh/2.))
       #lx=0 if lx<0 else lx
       #ly=0 if ly<0 else ly
       #if lx<0 or ly<0:continue
       print(lx,ly,rx,ry)
-      if classid == 17: image_np = cv2.rectangle(image_np,(lx,ly),(rx,ry),(0,0,255),1)
-      else: image_np = cv2.rectangle(image_np,(lx,ly),(rx,ry),(255,255,255),1)
+      image_np = cv2.rectangle(image_np,(lx,ly),(rx,ry),(255,255,255),1)
+#      if classid == 17: image_np = cv2.rectangle(image_np,(lx,ly),(rx,ry),(0,0,255),1)
+#      else: image_np = cv2.rectangle(image_np,(lx,ly),(rx,ry),(255,255,255),1)
       #set_trace()
   cv2.imshow('demo',image_np)
   while True:
     if cv2.waitKey(10)!=-1:break
-  set_trace()
-  sys.exit(-1)
-  # Visualization of the results of a detection.
-  vis_util.visualize_boxes_and_labels_on_image_array(
-      image_np,
-      output_dict['detection_boxes'],
-      output_dict['detection_classes'],
-      output_dict['detection_scores'],
-      category_index,
-      instance_masks=output_dict.get('detection_masks'),
-      use_normalized_coordinates=True,
-      line_thickness=8)
-  #plt.figure(figsize=IMAGE_SIZE)
-  #plt.imshow(image_np)
-  cv2.imshow('image',cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
-  print(image_path)
-  while True:
-    if cv2.waitKey(1)!=-1:break
+#  set_trace()
+#  sys.exit(-1)
+#  # Visualization of the results of a detection.
+#  vis_util.visualize_boxes_and_labels_on_image_array(
+#      image_np,
+#      output_dict['detection_boxes'],
+#      output_dict['detection_classes'],
+#      output_dict['detection_scores'],
+#      category_index,
+#      instance_masks=output_dict.get('detection_masks'),
+#      use_normalized_coordinates=True,
+#      line_thickness=8)
+#  #plt.figure(figsize=IMAGE_SIZE)
+#  #plt.imshow(image_np)
+#  cv2.imshow('image',cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR))
+#  print(image_path)
+#  while True:
+#    if cv2.waitKey(1)!=-1:break
   print("Next")
 
